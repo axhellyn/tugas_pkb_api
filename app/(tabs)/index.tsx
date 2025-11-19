3c3f06f1b4d87a7ffa5a64b381d04bf3b0bf3186
mhasaero <eroouji7314@gmail.com>
2025-11-19 22:02:09 +0700

    feat: add pull to refresh

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,17 @@
 import fetchLocation from "@/services/fetchLocation";
 import { Feather } from "@expo/vector-icons";
 import { LinearGradient } from "expo-linear-gradient";
-import React, { useEffect, useState } from "react";
-import { Pressable, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback, useEffect, useState } from "react";
+import { RefreshControl, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
-import PullToRefreshExampleTS from "@/components/refresh";
 
 import HeaderApp from "@/components/HeaderApp";
 
 export default function HomeScreen() {
   const [data, setData] = useState<any | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [refreshing, setRefreshing] = useState(false);
   const [error, setError] = useState<Error | null>(null);
 
   const formatUpdate = (timestamp: any) => {
@@ -32,79 +32,101 @@ export default function HomeScreen() {
   };
 
   const getLocationData = async () => {
-    setIsLoading(true);
-    setError(null);
     try {
       const response = await fetchLocation();
       setData(response);
     } catch (e: any) {
       setError(e);
-    } finally {
-      setIsLoading(false);
     }
   };
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    try {
+      const response = await fetchLocation();
+      setData(response);
+      setError(null);
+    } catch (e: any) {
+      setError(e);
+    } finally {
+      setRefreshing(false);
+    }
+  }, []);
 
   useEffect(() => {
     getLocationData();
   }, []);
 
-  // if (isLoading) {
-  //   return (
-  //     <LinearGradient
-  //       colors={["#1F2937", "#111827"]}
-  //       className="flex-1 items-center justify-center"
-  //     >
-  //       <ActivityIndicator size="large" color="#FFFFFF" />
-  //       <Text className="text-white text-lg mt-4">Memuat data...</Text>
-  //     </LinearGradient>
-  //   );
-  // }
-
   if (error) {
     return (
-      <LinearGradient
-        colors={["#991B1B", "#7F1D1D"]}
-        className="flex-1 items-center justify-center p-5s"
-      >
-        <Feather name="alert-triangle" size={48} color="white" />
-        <Text className="text-white text-2xl font-bold mt-4">
-          Terjadi Kesalahan{" "}
-        </Text>
-        <Text className="text-white text-center mt-2">{error?.message}</Text>
-        <Text className="text-white text-center mt-2">{error.message}</Text>
+      <LinearGradient colors={["#991B1B", "#7F1D1D"]} className="flex-1">
+        <SafeAreaView className="flex-1" edges={["left", "right", "bottom"]}>
+          <ScrollView
+            contentContainerStyle={{
+              flexGrow: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            refreshControl={
+              <RefreshControl
+                refreshing={refreshing}
+                onRefresh={onRefresh}
+                tintColor="#ffffff"
+              />
+            }
+          >
+            <Feather name="alert-triangle" size={48} color="white" />
+            <Text className="text-white text-2xl font-bold mt-4">
+              Terjadi Kesalahan
+            </Text>
+            <Text className="text-white text-center mt-2 px-4">
+              {error?.message}
+            </Text>
+          </ScrollView>
+        </SafeAreaView>
       </LinearGradient>
     );
   }
 
   return (
-    <>
-      <HeaderApp title="KAJURTREX" />
-      <View className="flex justify-center items-center w-full">
-        <PullToRefreshExampleTS />
-        <LinearGradient
-          colors={["#3b82f6", "#1d4ed8"]}
-          // kalau anda ios, komen di baris bawah unkomenin
-          // className="absolute left-0 right-0 bottom-0 top-0 w-full"
+    <LinearGradient colors={["#3b82f6", "#1d4ed8"]} className="flex-1">
+      <StatusBar hidden={true} />
+
+      <SafeAreaView className="flex-1" edges={["left", "right", "bottom"]}>
+        <ScrollView
+          stickyHeaderIndices={[0]}
+          contentContainerStyle={{ flexGrow: 1 }}
+          refreshControl={
+            <RefreshControl
+              refreshing={refreshing}
+              onRefresh={onRefresh}
+              tintColor="#ffffff"
+              colors={["#1d4ed8"]}
+              progressViewOffset={40}
+            />
+          }
         >
-          <SafeAreaView className="flex-1 items-center justify-center p-8 w-full">
-            <Text className="text-white text-3xl font-bold mb-8">
-              Lacak Lokasi Kajur
-            </Text>
+          <HeaderApp title="KAJURTREX" />
+
+          <View className="flex-1 justify-center items-center p-8 mt-20">
+            <View className="mb-5">
+              <Text className="text-3xl font-extrabold text-white">
+                Lacak Lokasi Kajur
+              </Text>
+            </View>
             <View className="bg-white/95 min-w-[340px] items-center rounded-3xl p-8 shadow-2xl">
               <View className="bg-blue-100 p-5 rounded-full mb-6 border-4 border-blue-200">
                 <Feather name="map-pin" size={48} color="#1d4ed8" />
               </View>
-              <View className=" flex flex-row items-center justify-center">
+
+              <View className="flex flex-row items-center justify-center">
                 <Text className="text-gray-500 text-sm uppercase tracking-wider font-semibold">
                   Lokasi Terdeteksi
                 </Text>
-                <Pressable onPress={getLocationData} className="p-4 ">
-                  <Feather name="refresh-cw" size={24} color="#1d4ed8" />
-                </Pressable>
               </View>
 
               <Text className="text-gray-900 text-4xl font-extrabold my-2 text-center">
-                {data?.location}
+                {data?.location || "Menunggu Data..."}
               </Text>
 
               <View className="bg-gray-100 px-4 py-2 rounded-full mt-6">
@@ -113,9 +135,9 @@ export default function HomeScreen() {
                 </Text>
               </View>
             </View>
-          </SafeAreaView>
-        </LinearGradient>
-      </View>
-    </>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
